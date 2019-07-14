7f7c5bec78cbbffe574a753ebe14d9ea4565c842
AndreSFND <49792739+AndreSFND@users.noreply.github.com>
2019-07-14 19:00:02 -0300

    Walking and Camera
    +Início do projeto
    +Movimentação do personagem
    +Câmera seguindo o personagem

---
+++ Pesadelho/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+
+	public Transform target;
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+
+    	Vector3 desiredPosition 	= new Vector3(target.position.x + offset.x, target.position.y + offset.y, -1);
+    	Vector3 smoothedPosition 	= Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+    	transform.position = smoothedPosition;
+        
+    }
+}
---
+++ Pesadelho/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	Rigidbody2D _rb;
+	float _horizontalInput, _verticalInput;
+
+	public float speed;
+
+	void Awake(){
+
+		_rb = GetComponent<Rigidbody2D>();
+
+	}
+
+    // Start is called before the first frame update
+    void Start(){
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    	//GetAxisRaw has no smoothing
+    	_horizontalInput 	= Input.GetAxisRaw("Horizontal");
+        _verticalInput 		= Input.GetAxisRaw("Vertical");
+
+        _rb.velocity = new Vector2(_horizontalInput*speed, _verticalInput*speed);
+
+    }
+
+}
