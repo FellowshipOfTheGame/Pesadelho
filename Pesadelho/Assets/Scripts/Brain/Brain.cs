707b9bd5c48262a9e9b883b00331c6802aa84d47
Vitor <vitoravian@usp.br>
2019-08-13 17:30:05 -0300

    Cérebro feito
    Entidade do cérebro feito, quando um inimigo encosta ele perde vida. Se vida chegar a 0 da game over

---
+++ Pesadelho/Assets/Scripts/Brain/Brain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    [SerializeField] private int health;
+    public int Health { get => health; set => health = value; }
+    [SerializeField]private GameObject GameOverCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameOverCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag == "Enemy"){
+            TakeDamage();
+            Destroy(col.gameObject);
+        }
+    }
+
+    private void GameOver(){
+        Time.timeScale = 0;
+        GameOverCanvas.SetActive(true);
+    }
+    public void TakeDamage(){
+        health--;
+        if(health <= 0){
+            GameOver();
+        }
+    }
+}
