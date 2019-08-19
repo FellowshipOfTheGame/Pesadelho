b80e8410f117c05a5e7a5a728b95c38a9e122264
Vitor <vitoravian@usp.br>
2019-08-19 12:43:59 -0300

    Começo das waves
    Descobrindo como funciona as waves

--- Pesadelho/Assets/Scripts/Brain/Brain.cs
+++ Pesadelho/Assets/Scripts/Brain/Brain.cs
@@ -20,17 +20,23 @@ public class Brain : MonoBehaviour
         
     }
 
+    //Quando algum objeto entra em colisão com o cérebro
      void OnCollisionEnter2D(Collision2D col){
+        //Se a tag desse objeto for Enemy chamar a função de tomar dano
+        // e destruir o inimigo que estiver atacando
         if(col.gameObject.tag == "Enemy"){
             TakeDamage();
             Destroy(col.gameObject);
         }
     }
 
+    //Função para parar o jogo e mostrar a tela de game over
     private void GameOver(){
         Time.timeScale = 0;
         GameOverCanvas.SetActive(true);
     }
+    
+    //Função para subtrair vida do cérebro
     public void TakeDamage(){
         health--;
         if(health <= 0){
---
+++ Pesadelho/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawn());
+        
+    }
+
+    IEnumerator spawn(){
+        for(int i = 0; i<5; i++){
+            Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity).SetActive(true);
+            yield return new WaitForSeconds(3);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
