292b67adcddfc4aa227c27f542187f47b3a66850
andre_sfnd <andre_sfnd@usp.br>
2019-12-03 16:01:42 -0300

    Muda layer

---
+++ Pesadelho/Assets/Scripts/MudaLayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudaLayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+
+        if(col.tag == "Player"){
+
+            col.GetComponent<SpriteRenderer>().sortingLayerName  = "PlayerFundo";
+
+        }
+
+    }
+    
+    void OnTriggerExit2D(Collider2D col){
+
+        if(col.tag == "Player"){
+
+            col.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
+
+        }
+
+    }
+}
